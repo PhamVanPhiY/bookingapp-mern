27aebee376d1e3dcc029feaf6c706fa0ce2fa3e8
Phạm Văn Phi <93720543+PhamVanPhiY@users.noreply.github.com>
2023-06-27 21:48:34 +0700

    Finished AccountPage

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -4,60 +4,59 @@ import { Link, Navigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function AccountPage() {
-  // const [redirect, setRedirect] = useState(null);
+  const [redirect, setRedirect] = useState(null);
   const { user, ready, setUser } = useContext(UserContext);
-  // let { subpage } = useParams();
-  // if (subpage === undefined) {
-  //   subpage = "profile";
-  // }
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
 
-  // async function logout() {
-  //   await axios.post("/logout");
-  //   setUser(null);
-  //   setRedirect("/");
-  // }
+  async function logout() {
+    await axios.post("/logout");
+    setUser(null);
+    setRedirect("/");
+  }
 
-  // if (!ready) {
-  //   return "Loading";
-  // }
-  // if (ready && !user) {
-  //   return <Navigate to={"/login"} />;
-  // }
+  if (!ready) {
+    return "Loading";
+  }
+  if (ready && !user) {
+    return <Navigate to={"/login"} />;
+  }
 
-  // function linkClasses(type = null) {
-  //   let classes = "py-2 px-6";
-  //   if (type === subpage) {
-  //     classes += " bg-primary text-white rounded-full";
-  //     console.log(user);
-  //   }
-  //   return classes;
-  // }
-  // if (redirect) {
-  //   return <Navigate to={redirect}></Navigate>;
-  // }
-  // return (
-  //   <div>
-  //     <nav className="w-full flex mt-8 justify-center  gap-2 mb-8">
-  //       <Link className={linkClasses("profile")} to={"/account"}>
-  //         My profile
-  //       </Link>
-  //       <Link className={linkClasses("bookings")} to={"/account/bookings"}>
-  //         My bookings
-  //       </Link>
-  //       <Link className={linkClasses("places")} to={"/account/places"}>
-  //         My places
-  //       </Link>
-  //     </nav>
+  function linkClasses(type = null) {
+    let classes = "py-2 px-6";
+    if (type === subpage) {
+      classes += " bg-primary text-white rounded-full";
+      console.log(user);
+    }
+    return classes;
+  }
+  if (redirect) {
+    return <Navigate to={redirect}></Navigate>;
+  }
+  return (
+    <div>
+      <nav className="w-full flex mt-8 justify-center  gap-2 mb-8">
+        <Link className={linkClasses("profile")} to={"/account"}>
+          My profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          My bookings
+        </Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>
+          My places
+        </Link>
+      </nav>
 
-  //     {subpage === "profile" && (
-  //       <div className="text-center max-w-lg mx-auto">
-  //         Logged in as {user.data.name} ({user.data.email})
-  //         <button onClick={logout} className="primary max-w-sm mt-2">
-  //           Logout
-  //         </button>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-  return <div>account page for {user.name}</div>;
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.namne} ({user.email})
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
